48cac7abaec56774aa002f7755da988a11ff7391
Epistememos <signup.yy@gmail.com>
2025-02-02 12:57:26 -0500

    We shall come back at a later time, for he who gives up has definitely lost

--- main.py
+++ main.py
@@ -8,8 +8,12 @@ from io import StringIO
 
 load_dotenv()
 
+
 client = OpenAI(api_key=os.getenv("DEEPSEEK_API_KEY"), base_url="https://api.deepseek.com")
 
+
+df = pd.read_csv('apache_error_parsed.csv').head(10)
+csv_string = df.to_string(index=False)
 training_args = SFTConfig(
     max_seq_length=512,
     output_dir="/tmp",
@@ -26,16 +30,6 @@ def get_response(system_prompt, user_prompt):
     )
     return response.choices[0].message.content
 
-dataset_str = (get_response("You're a movie critic", "Generate 50 movie reviews and tell me if its negative (0) or positive (1) and output that in a csv format where a row looks like: <review>, <0 or 1> (Don't forget the column names)"))
-df = pd.read_csv(StringIO(dataset_str))
+dataset_str = (get_response("You're a senior developer, propose valid solutions for each error logs and link them up", csv_string))
 
-dataset = Dataset.from_pandas(df)
-
-
-trainer = SFTTrainer(
-    "facebook/opt-350m",
-    train_dataset=dataset,
-    args=training_args,
-)
-
-trainer.train()
+print(dataset_str)
